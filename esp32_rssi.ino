40096be77779a6a79c24e25ce66b93f9b6e0bc8c
Hedayet Islam <islamhedayet67@gmail.com>
2020-11-09 18:57:16 +0600

    first commit

---
+++ esp32_rssi.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+#include <WiFi.h>
+
+char* ssid = "STELLAR";
+char* pass = "stellarBD";
+
+//char* ssid = "Home_TP";
+//char* pass = "llIlIIl0O~`IlIl";
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Couldn't get a wifi connection");
+    WiFi.begin(ssid, pass);
+    delay(2000);
+  }
+  // if you are connected, print out info about the connection:
+ 
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("RSSI:");
+    Serial.println(rssi);
+  
+}
+
+void loop () {
+  Serial.println(WiFi.RSSI());
+  
+//  Serial.println(WiFi.RSSI('192.168.0.107'));
+  delay(100);
+}
